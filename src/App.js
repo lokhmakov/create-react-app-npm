590dde02ff9465a1f0c2c800d85745811add8c3e
lokhmakov <l.pavel.m@gmail.com>
2017-04-10 14:08:50 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React                from 'react'
+
+import YourComponent        from 'components/YourComponent'
+
+class App extends React.Component {
+  render() {
+    return (
+      <YourComponent>
+        1
+      </YourComponent>
+    )
+  }
+}
+
+export default App
